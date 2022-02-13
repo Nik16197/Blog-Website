56ca2113596e711fe6051d33e78bd72033cffa23
Nikhil <niks16197@gmail.com>
2022-02-13 18:38:37 +0530

    Context

--- Blog/client/src/pages/login/login.jsx
+++ Blog/client/src/pages/login/login.jsx
@@ -1,16 +1,45 @@
+import axios from "axios";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./login.css";
+import { useRef } from "react";
+import { Context } from "../../context/context";
 
 export default function Login() {
+
+    const userRef = useRef();
+
+
+    const { user, dispatch, isFetching } = useContext(Context);
+
+    const handleSubmit = (e) => {
+        console.log(e);
+        e.preventDefault();
+
+    }
+
+
     return (
         <div className="login">
             <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
-                <input className="loginInput" type="text" placeholder="Enter your email..." />
+            <form className="loginForm" onSubmit={handleSubmit}>
+
+                <label>Username</label>
+                <input className="loginInput"
+                    type="text"
+                    placeholder="Enter your username..."
+                    ref={userRef}
+                />
+
                 <label>Password</label>
-                <input className="loginInput" type="password" placeholder="Enter your password..." />
-                <button className="loginButton">Login</button>
+                <input className="loginInput"
+                    type="password"
+                    placeholder="Enter your password..."
+
+                />
+
+                <button className="loginButton" type="submit">Login</button>
+
             </form>
             <button className="loginRegisterButton">
                 <Link to="/register" className="link">Register</Link>
